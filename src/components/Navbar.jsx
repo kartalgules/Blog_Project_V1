99265848babb1d7a474234b5e15de0c30d21fcb1
Kartal Gules <kartalgules@gmail.com>
2023-12-21 22:54:03 +0300

    Navbar edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+// import { NavLink } from 'react-router-dom'
+// import Logout from '../components/Logout'
 import "../styles/Navbar.css";
 
 function Navbar() {
@@ -7,7 +9,11 @@ function Navbar() {
         <div className="logo">
           <h2>BLOG</h2>
         </div>
-        <div className="navbar"></div>
+        <div className="navbar">
+            {/* <NavLink to="/">Home</NavLink>
+            <NavLink to="">Blogs</NavLink>
+            <NavLink to={<Logout />}>Logout</NavLink> */}
+        </div>
       </div>
     </div>
   );
